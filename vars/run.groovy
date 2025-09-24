f8aff1d86c1ab32d8a3197e7da9f44f7a5988d09
Krishna Pathak <88155252+KrishnaPathak824@users.noreply.github.com>
2025-09-24 13:11:26 +0545

    Add Docker management function in run.groovy

---
+++ vars/run.groovy
@@ -0,0 +1,8 @@
+def call(String image, String container){
+sh """
+    docker pull ${image}:latest
+    docker stop ${container}
+    docker rm ${container}
+    docker run --name ${container} -d -p 8080:80 -e VITE_CLERK_PUBLISHABLE_KEY=$KEY ${image}:latest
+"""
+}
