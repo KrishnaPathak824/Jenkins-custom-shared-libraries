21c59e2ff4cc8a5cfecce14db87e339b109505f7
Krishna Pathak <88155252+KrishnaPathak824@users.noreply.github.com>
2025-10-09 17:18:46 +0545

    Update run.groovy

--- vars/run.groovy
+++ vars/run.groovy
@@ -1,8 +1,12 @@
 def call(String image, String container){
-sh """
-    docker pull jhinga/${image}:latest
-    docker stop ${container}
-    docker rm ${container}
-    docker run --name ${container} -d -p 8080:80 -e VITE_CLERK_PUBLISHABLE_KEY=$KEY ${image}:latest
-"""
+    sh """
+        docker pull jhinga/${image}:latest
+
+        if [ \$(docker ps -a -q -f name=^/${container}\$) ]; then
+            docker stop ${container}
+            docker rm ${container}
+        fi
+
+        docker run --name ${container} -d -p 8080:80 -e VITE_CLERK_PUBLISHABLE_KEY=\$KEY jhinga/${image}:latest
+    """
 }
