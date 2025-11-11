143b5ca2e9273c1c50ca35e764662dd48df2eb11
Krishna Pathak <88155252+KrishnaPathak824@users.noreply.github.com>
2025-11-11 22:35:03 +0545

    Update sonarqube.groovy

--- vars/sonarqube.groovy
+++ vars/sonarqube.groovy
@@ -1,5 +1,5 @@
 def call(String SonarQubeAPI, String Project, String ProjectKey){
   withSonarQubeEnv("${SonarQubeAPI}"){
-    sh "$SONAR_HOME/bin/sonar-scanner -Dsonar.projectName=${Project} -Dsonar.projectkey=${ProjectKey} -X"
+    sh "$SONAR_HOME/bin/sonar-scanner -Dsonar.projectName=${Project} -Dsonar.projectKey=${ProjectKey} -X"
   }
 }
