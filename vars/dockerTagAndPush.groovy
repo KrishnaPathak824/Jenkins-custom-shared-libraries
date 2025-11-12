17d1083e28a3c1deef8b7421ccf4345688f5dd08
Krishna Pathak <88155252+KrishnaPathak824@users.noreply.github.com>
2025-11-12 20:39:29 +0545

    Update dockerTagAndPush.groovy

--- vars/dockerTagAndPush.groovy
+++ vars/dockerTagAndPush.groovy
@@ -1,10 +1,10 @@
-def call(String Project){
+def call(String Project, String Version){
 withCredentials([usernamePassword('credentialsId':"docker-cred",passwordVariable:"dockerpass",usernameVariable:"dockeruser")]){
                     sh """
                     docker login -u $dockeruser -p $dockerpass
-                    docker image tag ${Project} $dockeruser/${Project}:${BUILD_ID}
+                    docker image tag ${Project} $dockeruser/${Project}:${Version}
                     docker image tag ${Project} $dockeruser/${Project}:latest
-                    docker push $dockeruser/${Project}:${BUILD_ID}
+                    docker push $dockeruser/${Project}:${Version}
                     docker push $dockeruser/${Project}:latest
                     """
                     }
